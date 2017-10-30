ab8daa60cdadab35153a0d29c0ef548eea688173
Kevin Robinson <kevinr.zan@gmail.com>
2017-10-30 13:15:54 -0700

    start deque implementation

---
+++ deque.py
@@ -0,0 +1,37 @@
+'''.'''
+
+
+class Deque(object):
+
+    def __init__(self):
+        pass
+
+    def append(self, val):
+        pass
+
+    def appendleft(self, val):
+        pass
+
+    def pop(self):
+        pass
+
+    def popleft(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def peekleft(self):
+        pass
+
+    def size(self):
+        pass
+
+    def __len__(self):
+        pass
+
+
+class Node(object):
+
+    def __init__(self, value):
+        pass
